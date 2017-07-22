34046844c58ee64a0bc4b0bbde973b63aa96f129
crynobone <crynobone@gmail.com>
2017-07-22 18:01:55 +0800

    Prepare profiler.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/ProfilerServiceProvider.php
+++ src/ProfilerServiceProvider.php
@@ -6,5 +6,32 @@ use Illuminate\Support\ServiceProvider;
 
 class ProfilerServiceProvider extends ServiceProvider
 {
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
 
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('laravie.profiler', function ($app) {
+            return new Profiler($app->make('log')->getMonolog());
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['laravie.profiler'];
+    }
 }
